01ba1d95e8d782d87b92d21d0ac77d8933b4e4fa
Clemens Hahn <clemens@couchbits.com>
2023-06-20 08:20:32 +0200

    load app configuration from file

--- src/common/runtime_configuration.R
+++ src/common/runtime_configuration.R
@@ -1,5 +1,5 @@
 configuration <- function() {
-    configurationString <- Sys.getenv(x = "CONFIGURATION", "{}")
+    configurationString <- Sys.getenv(x = "CONFIGURATION_FILE", "{}")
 
     result <- if(configurationString != "") {
         jsonlite::fromJSON(txt=configurationString)
@@ -12,10 +12,4 @@ configuration <- function() {
         logger.info("app will be started with configuration:\n%s", jsonlite::toJSON(result, auto_unbox = TRUE, pretty = TRUE))
     }
     result
-}
-
-storeConfiguration <- function(configuration) {
-  jsonlite::write_json(configuration, "./data/output/configuration.json", auto_unbox = TRUE)
-  Sys.setenv(CONFIGURATION = jsonlite::toJSON(configuration, auto_unbox = TRUE))
-  logger.info("Stored configuration of shinyModule to 'configuration.json'")
 }
