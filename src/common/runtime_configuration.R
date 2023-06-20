b1f2a1c52abcab0898f5453e0382a4832192f957
Clemens Hahn <clemens@couchbits.com>
2023-06-20 11:11:29 +0200

    housekeeping

--- src/common/runtime_configuration.R
+++ src/common/runtime_configuration.R
@@ -1,14 +1,14 @@
 configuration <- function() {
-    configurationString <- Sys.getenv(x = "CONFIGURATION_FILE", "{}")
+    configurationFile <- Sys.getenv(x = "CONFIGURATION_FILE", "")
 
-    result <- if(configurationString != "") {
-        jsonlite::fromJSON(txt=configurationString)
+    result <- if(configurationFile != "") {
+        jsonlite::fromJSON(txt=configurationFile)
     } else {
         NULL
     }
 
     if (Sys.getenv(x = "PRINT_CONFIGURATION", "no") == "yes") {
-        logger.debug("parse stored configuration: \'%s\'", configurationString)
+        logger.debug("parse stored configuration: \'%s\'", configurationFile)
         logger.info("app will be started with configuration:\n%s", jsonlite::toJSON(result, auto_unbox = TRUE, pretty = TRUE))
     }
     result
