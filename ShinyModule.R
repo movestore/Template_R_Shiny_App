5606d5fc86970992a063ddd4bc3ae0689c1708bc
Clemens Hahn <clemens@couchbits.com>
2023-06-20 08:19:41 +0200

    code style

--- ShinyModule.R
+++ ShinyModule.R
@@ -2,8 +2,8 @@ library("shiny")
 library("move")
 
 # to display messages to the user in the log file of the App in MoveApps
-# one can use the function from the src/common/logger.R file:
-# logger.fatal() -> logger.trace()
+# one can use the function from the logger.R file:
+# logger.info(). Available levels are error(), warn(), info(), debug(), trace()
 
 shinyModuleUserInterface <- function(id, label) {
   ns <- NS(id) ## all IDs of UI functions need to be wrapped in ns()
@@ -14,13 +14,15 @@ shinyModuleUserInterface <- function(id, label) {
   )
 }
 
-shinyModule <- function(input, output, session, data){ ## The parameter "data" is reserved for the data object passed on from the previous app
-  ns <- session$ns ## all IDs of UI functions need to be wrapped in ns()
+# The parameter "data" is reserved for the data object passed on from the previous app
+shinyModule <- function(input, output, session, data) {
+  # all IDs of UI functions need to be wrapped in ns()
+  ns <- session$ns
   current <- reactiveVal(data)
-  
+
   output$plot <- renderPlot({
     plot(data[[input$ind]])
   })
-  
-  return(reactive({ current() })) ## if data are not modified, the unmodified input data must be returned
+  # if data are not modified, the unmodified input data must be returned
+  return(reactive({ current() }))
 }
