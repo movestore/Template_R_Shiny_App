3858ce59d654d99d39c004134e9366422d32b9f9
Clemens Hahn <clemens@couchbits.com>
2023-08-21 10:13:03 +0200

    io-type independency

--- src/io/io_handler.R
+++ src/io/io_handler.R
@@ -1,14 +1,9 @@
 readInput <- function(sourceFile) {
-    inputType <- Sys.getenv(x = "INPUT_TYPE", "move::moveStack")
     # for now every input-type can be read via RDS
-    logger.debug("Read input of type '%s'", inputType)
     return(readRdsInput(sourceFile))
 }
 
 storeResult <- function(result, outputFile) {
-    outputType <- Sys.getenv(x = "OUTPUT_TYPE", "move::moveStack")
-    # for now every output-type can be written via RDS
-    logger.debug("Write output of type '%s'", outputType)
     return(storeRdsOutput(result, outputFile))
 }
 
@@ -37,8 +32,4 @@ errorFile <- function() {
   result <- Sys.getenv(x = "ERROR_FILE", "")
   logger.debug("errorFile: %s", result)
   result
-}
-
-notifyDone <- function(executionRuntime) {
-    logger.trace("I'm ready.")
 }
